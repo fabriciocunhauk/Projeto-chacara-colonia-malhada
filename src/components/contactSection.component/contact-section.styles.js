f246f8650e035f3979f182cf4d6f522459f4a65e
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2021-02-13 03:56:39 +0000

    Contact header

--- src/components/contactSection.component/contact-section.styles.js
+++ src/components/contactSection.component/contact-section.styles.js
@@ -3,4 +3,9 @@ import styled from 'styled-components';
 export const ContactSectionContainer = styled.div`
     width: 100vw;
     height: 100vh;
+    text-align: center;
+
+    h1 {
+        margin-top: 90px;
+    }
 `;
